3feeffcb54b6f2b7ee06bf613ff8b28b980a2cfd
Priya Moghe <102720207+pizuCoder@users.noreply.github.com>
2022-11-19 12:50:18 +0530

    done

---
+++ Sharpener Practice-2/index.js
@@ -0,0 +1,10 @@
+const headerTitle = document.getElementById('header-title')
+const header = document.getElementById('main-header')
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'Goodbye';
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = '<h3>Hello</h3>';
+header.style.borderBottom = 'solid 3px #000'
+document.getElementById('main').children[0].style.fontWeight='bold'
+document.getElementById('main').children[0].style.color = 'green'
