a7e68f250e5b5806c1d210e83c74dc7b9fc9d54d
Priya Moghe <102720207+pizuCoder@users.noreply.github.com>
2022-11-25 18:02:02 +0530

    Ticket Booker app that stores all userdata in localStorage

---
+++ ticketbooker/index.js
@@ -0,0 +1,83 @@
+const form = document.getElementById('addForm');
+const itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+// const submitBtn = document.getElementById('Submit')
+// Form submit event
+form.addEventListener('submit', addItem);
+
+//delete item
+itemList.addEventListener('click', removeItem);
+//filter
+filter.addEventListener('keyup', filterItems);
+
+
+// Add item
+function addItem(e){
+e.preventDefault();
+// Get input value
+let newItem = document.getElementById('item').value 
+let description = document.getElementById('description').value
+// Create new li element
+let li= document.createElement('li');
+// Add class
+li.className = 'list-group-item';
+// Add text node with input value
+let itemstoAppend = document.createTextNode (newItem + " " + description)
+li.appendChild(itemstoAppend)
+// li.appendChild(document.createTextNode (description))
+
+
+//localStorage Addition
+function addtoLocalStorage(){
+    let existingItems = JSON.parse(localStorage.getItem("allListItems"))
+    if(existingItems == null) existingItems = []
+    let userName = document.createTextNode(newItem)
+    let userEmail = document.createTextNode (description)
+    let itemsObj = {
+        "Name": userName.textContent,
+        "Email": userEmail.textContent
+    }
+    localStorage.setItem("ListItem", JSON.stringify(itemsObj))
+    existingItems.push(itemsObj)
+    localStorage.setItem("allListItems", JSON.stringify(existingItems))
+    // let itemsObj_seralized = JSON.stringify(itemsObj)
+    
+    // localStorage.setItem("ListItem",itemsObj_seralized)
+    // let itemsObj_deserailzed = JSON.parse(localStorage.getItem("ListItem"))
+    // console.log(itemsObj_deserailzed)
+
+}
+addtoLocalStorage()
+
+itemList.appendChild(li);
+document.getElementById('item').value = ''
+document.getElementById('description').value = ''
+
+}
+
+// Remove item
+function removeItem(e){
+    if(e.target.classList.contains('delete')) {
+        if (confirm('Are You Sure?')){
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
+
+// Filter Items
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get list
+    var items=  itemList.getElementsByTagName('li')
+    // Convert to an array
+    Array.from(items).forEach(function(item) {
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+        } else {
+        item.style.display = 'none';
+        }
+    })
+    }
